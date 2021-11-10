0b6fe06e8cf69d34147a33449c4fbad8c7b9375b
Junho Park <bakjun94@gmail.com>
2021-11-10 10:52:44 +0900

    second commit

---
+++ .vscode/combine.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+typedef long long ll
+
+int main(){
+	ll n;
+	cin>>n;
+	vector<vector<ll>> v(n);
+	for(int i=0;i<n;i++){
+		int tmp;
+		cin>>tmp;
+		for(int j=0;j<tmp;j++){
+			int tp;
+			cin>>tp;
+			v[i].push_back(tp);
+		}
+	}
+	for(int i=0;i<){}
+} 
