6b7d91eec78e883da2742605e3b5c5e5b80afff9
Junho Park <bakjun94@gmail.com>
2021-11-11 16:55:41 +0900

    com2 commit

--- .vscode/boj/combine.cpp
+++ .vscode/boj/combine.cpp
@@ -7,24 +7,25 @@ ll min1;
 //실패한 파일합치기
 
 ll sum(vector<ll> v ,ll l,ll r){
-    
-    if(r-l==0){
-        return v[l];
-    }
-    if(r-l==1){
-        return v[l]+v[r];
-    }
     ll all=0;
     for(ll i=l;i<r+1;i++){
         all+=v[i];
     }
-    
-    ll sumt=sum(v,l,r-1)+all;
-    if(sumt>sum(v,l+1,r)+all)
-        sumt=sum(v,l+1,r)+all;
-    if(sumt>sum(v,l,(l+r)/2)+sum(v,(l+r)/2+1,r)+all)
-        sumt=sum(v,l,(l+r)/2)+sum(v,(l+r)/2+1,r)+all;
-    return sumt;
+    if(r-l==0){
+        return v[l];
+    }
+    else if(r-l==1){
+        return v[l]+v[r];
+    }
+
+    else{
+        ll sumt=sum(v,l,r-1)+all;
+        if(sumt>sum(v,l+1,r)+all)
+            sumt=sum(v,l+1,r)+all;
+        if(sumt>sum(v,l,(l+r)/2)+sum(v,(l+r)/2+1,r)+all)
+            sumt=sum(v,l,(l+r)/2)+sum(v,(l+r)/2+1,r)+all;
+        return sumt;
+    }
 }
 
 int main(){
