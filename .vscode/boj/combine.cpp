2c74f06035ee0f6929d847451dcbe7d00621f526
Junho Park <bakjun94@gmail.com>
2021-11-11 16:50:18 +0900

    combine1 commit

--- .vscode/boj/combine.cpp
+++ .vscode/boj/combine.cpp
@@ -2,8 +2,30 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-
 typedef long long ll;
+ll min1;
+//실패한 파일합치기
+
+ll sum(vector<ll> v ,ll l,ll r){
+    
+    if(r-l==0){
+        return v[l];
+    }
+    if(r-l==1){
+        return v[l]+v[r];
+    }
+    ll all=0;
+    for(ll i=l;i<r+1;i++){
+        all+=v[i];
+    }
+    
+    ll sumt=sum(v,l,r-1)+all;
+    if(sumt>sum(v,l+1,r)+all)
+        sumt=sum(v,l+1,r)+all;
+    if(sumt>sum(v,l,(l+r)/2)+sum(v,(l+r)/2+1,r)+all)
+        sumt=sum(v,l,(l+r)/2)+sum(v,(l+r)/2+1,r)+all;
+    return sumt;
+}
 
 int main(){
 	ll n, add=0;
@@ -17,18 +39,11 @@ int main(){
 			cin>>tp;
 			v[i].push_back(tp);
 		}
-        sort(v[i].begin(),v[i].end());
 	}
-
+    
 	for(int i=0;i<n;i++){
-        add=v[i][0]*(v[i].size()-1);
-        cout<<add<<" ";
-        for(int j=1;j<v[i].size();j++){
-            add=add+v[i][j]*(v[i].size()-j);
-            cout<<add<<" ";
-        }
-        
-        cout<<add<<endl;
+        cout<<sum(v[i],0,v[i].size()-1)<<endl;
     }
+
     return 0;
 } 
